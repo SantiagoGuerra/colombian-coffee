3d7ff57eabd4df593e39811a4e8eb46a66c77e07
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-11 22:44:53 -0500

    Se ha inicializado el proyecto

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `Café Colombiano`,
+    description: `Uno de las más deliciosas bebidas en toda la humanidad`,
+    author: `@santi_gueam`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `colombian coffee`,
+        short_name: `coffee`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import SEO from "../layouts/seo"
+
+const NotFoundPage = () => (
+  '404'
+)
+
+export default NotFoundPage
