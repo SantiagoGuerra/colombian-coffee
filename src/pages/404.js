0421e8c2e6067ee0361f2c93422f2026a02f78fb
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-12 20:47:34 -0500

    Se ha creado la pagina de redirección

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 import SEO from "../layouts/seo"
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
-  '404'
+  <div>
+  ' We do not found your page :( go here => '
+  <Link to='/'>Home</Link>
+  </div>
 )
 
 export default NotFoundPage
