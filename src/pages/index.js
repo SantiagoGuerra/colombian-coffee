2321a19f5b898657ad26569ab35484b9460bee0b
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-12 20:48:16 -0500

    Se ha crado la página principal para el proyecto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import '../styles/index.css'
+
 import SEO from "../layouts/seo"
+import Header from '../layouts/Header'
+import MainLayout from '../container/MainLayout'
+
+import WelSection from '../components/WelSection'
+import Card from "../components/Card";
+import MainContainer from "../components/MainContainer";
+
 
 const IndexPage = () => (
-    <div>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    </div>
+    <MainLayout>
+      <SEO title="Home" keywords={[`colombia`, `coffee`, `home`]} />
+      <WelSection />
+      <MainContainer>
+        <Card 
+          title='Te mantiene alerta!'
+          paragraph='Nada más tomar un café, la cafeína actúa en el cerebro, bloqueando un neurotransmisor, la adenosina, que hace que aumenten otras sustancias como la dopamina o la norepinefrina, que aceleran la actividad cerebral.'
+          src='https://image.flaticon.com/icons/svg/444/444187.svg'
+        />
+          <Card 
+          title='Mejora tu rendimiento físico'
+          paragraph='Muchos atletas toman varias tazas de café antes de competir, ya que la cafeína aumenta los niveles de adrenalina. Esta hormona prepara a nuestro cuerpo para un esfuerzo físico excepcional: hace que las células grasas descompongan la grasa corporal, liberándola como ácidos grasos libres, que usamos como combustible cuando hacemos ejercicio.'
+          src='https://image.flaticon.com/icons/svg/478/478729.svg'
+        />  <Card 
+        title='tiene nutrientes esenciales'
+        paragraph='Solemos pensar en el café como una simple mezcla de agua y cafeína, pero la infusión tiene muchos otros nutrientes esenciales para nuestro organismo. Una taza de café contiene riboflavina (vitamina B2), ácido pantoténico (vitamina B5), manganeso, potasio, magnesio y niacina.'
+        src='https://image.flaticon.com/icons/svg/478/478748.svg'
+      />
+      </MainContainer>
+    </MainLayout>
 )
 
 export default IndexPage
